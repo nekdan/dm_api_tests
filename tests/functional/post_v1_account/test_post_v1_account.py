a039b3a57a40251fd3afebd5b247cae0545e9e31
Nikita Danilushkin <nekdan@ya.ru>
2025-11-16 13:46:54 +0300

    add fixtures

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,4 +1,7 @@
+from collections import namedtuple
+from datetime import datetime
 
+import pytest
 import structlog
 
 from helpers.account_helper import AccountHelper
@@ -14,17 +17,42 @@ structlog.configure(
 )
 
 
-def test_post_v1_account():
+@pytest.fixture
+def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = ApiMailhog(configuration=mailhog_configuration)
+    return mailhog_client
+
+
+@pytest.fixture
+def account_api():
     dm_api_configuration =DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
-
     account = DMApiAccount(configuration=dm_api_configuration)
-    mailhog = ApiMailhog(configuration=mailhog_configuration)
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+    return account
 
-    login = 'n.danilushkin45'
-    email = f'{login}@mail.ru'
+
+@pytest.fixture
+def account_helper(account_api, mailhog_api):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
+
+
+@pytest.fixture
+def prepare_user():
+    now = datetime.now()
+    data = now.strftime('%d_%m_%Y_%H_%M_%S')
+    login = f'n.danilushkin_{data}'
     password = '123456'
+    email = f'{login}@mail.ru'
+    User = namedtuple('user', ['login', 'password', 'email'])
+    user = User(login=login, password=password, email=email)
+    return user
+
+
+def test_post_v1_account(account_helper, prepare_user):
+    login = prepare_user.login
+    email = prepare_user.email
+    password = prepare_user.password
 
     account_helper.register_new_user(login, password, email)
     account_helper.user_login(login, password)
